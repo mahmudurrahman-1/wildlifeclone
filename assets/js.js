419f01b602f1704fe9c57d02242c4fcf3c532073
mahmudurrahman-1 <rthe1761@gmail.com>
2022-01-25 20:22:14 +0600

    fixed

---
+++ assets/js.js
@@ -0,0 +1,9 @@
+function closeNav() {
+    document.getElementById("sidebar-close").style.display = "none";
+    document.getElementById("sidebar-open").style.display = "block";
+  }
+  function openNav() {
+    document.getElementById('sidebar-close').style.display="block";
+    document.getElementById("sidebar-open").style.display = "none";
+
+  }
